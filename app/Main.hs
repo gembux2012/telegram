fddff5f7d0e6a10ad3e4563abfa269151ffe25bf
alex <gembux2012@gmail.com>
2021-12-19 20:12:47 +0700

    current

--- app/Main.hs
+++ app/Main.hs
@@ -1,48 +1,51 @@
 {-# LANGUAGE AllowAmbiguousTypes   #-}
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BlockArguments        #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
 
 module Main where
 
-import App
-import Control.Error (ExceptT, runExceptT)
-import Control.Monad ()
-import Control.Monad.Reader (ReaderT, runReaderT, unless, asks)
-import Data.Has (getter)
-import Data.Maybe (fromMaybe)
-import Error
-import Data.Aeson (encode)
-import  qualified Data.ByteString.Char8 as BS8
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.Map as Map
-import Control.Monad.State ( evalStateT, modify, get, StateT, StateT, StateT)
-import Text.Read ()
-import Logger.Class
-import Control.Monad.Catch.Pure ()
-import qualified Data.Text as T
+import           App
+import           Control.Error                     (ExceptT, runExceptT)
+import           Control.Monad                     ()
+import           Control.Monad.Catch.Pure          ()
+import           Control.Monad.Reader              (ReaderT, asks, runReaderT,
+                                                    unless)
+import           Control.Monad.State               (StateT, evalStateT, get,
+                                                    modify)
+import           Data.Aeson                        (encode)
+import qualified Data.ByteString.Char8             as BS8
+import qualified Data.ByteString.Lazy              as LBS
+import           Data.Has                          (getter)
+import qualified Data.Map                          as Map
+import           Data.Maybe                        (fromMaybe)
+import qualified Data.Text                         as T
+import           Error
+import           Logger.Class
+import           Text.Read                         ()
 --import Logger.Types (Config(..), TelegramOpts(..), defaultLogOpts, Priority(..) )
-import Turtle (FilePath, encodeString)
-import Prelude hiding (FilePath,error,log)
-import Control.Concurrent (forkIO)
-import System.Directory.Internal.Prelude (exitFailure)
-import GHC.Base (error)
+import           Control.Concurrent                (forkIO)
+import           GHC.Base                          (error)
+import           Prelude                           hiding (FilePath, error, log)
+import           System.Directory.Internal.Prelude (exitFailure, hFlush, stdout)
+import           Turtle                            (FilePath, encodeString)
 --import Config
-import System.Exit (exitSuccess)
+import           System.Exit                       (exitSuccess)
 --import Types
-import Streams (initLog, msgLog, stopLog)
-import Turtle.Options (options)
-import Control.Monad.Cont (when, forever)
-import Turtle (printf)
-import Control.Concurrent.Async (race_)
-import Types
-import Config.Config
-import Config.Types
-import Logger.Types
-import Telegram.Api
+import           Config.Config
+import           Config.Types
+import           Control.Concurrent.Async          (race_)
+import           Control.Monad.Cont                (forever, when)
+import           Logger.Types
+import           Streams                           (initLog, msgLog, stopLog)
+import           Telegram.Api
+import           Turtle                            (printf)
+import           Turtle.Options                    (options)
+import           Types
+import Data.ByteString.Char8 (hPutStr)
 
 
 main =  startBot
@@ -50,40 +53,45 @@ main =  startBot
 startBot :: IO ()
 startBot  = do
   (Settings api config_path) <- options description settingsP
-  
+
   let list_user = Map.empty
-    
-  conf <- readConfig  config_path 
-  case conf of  
+
+  conf <- readConfig  config_path
+  case conf of
     Left  error -> do
      log <-initLog defaultLogOpts
-     msgLog log (fromLoggable INFO <> T.pack " bot abnormal termination ") 
+     msgLog log (fromLoggable INFO <> T.pack " bot abnormal termination ")
      msgLog log (fromLoggable ERROR <> T.pack " file : " <> T.pack error )
      stopLog log
-    Right config -> do  
+    Right config -> do
       log <-initLog (logOpts config)
       let logger = Logger $ msgLog log
       let api' = telegram
-      
+
       race_ ( runBot api' logger (telegramOpts config) list_user log) (do {
                                                                               forever $ do
+                                                                                hPutStr "bot:>"
+                                                                                hFlush stdout
                                                                                 arg <- getLine
+                                                                                putStrLn "kilkujlk"
                                                                                 case arg of
                                                                                   "stop" -> do
                                                                                     msgLog log (fromLoggable INFO <> T.pack " bot stopped ")
                                                                                     stopLog log
                                                                                     exitSuccess
-                                                                                  _ -> msgLog log (fromLoggable INFO <> T.pack " unknown commnd " <> T.pack arg) 
-                                                                              stopLog log})
+                                                                                  _ -> msgLog log (fromLoggable INFO <> T.pack " unknown commnd " <> T.pack arg)
+                                                                                return()
+                                                                              })
 
- 
-  
+
+
 runBot api logger config  list_user log =
  do
      res <- evalStateT (runReaderT (runExceptT  api ) (logger, config)) list_user
      case res of
        Left (BotError error) -> do
         msgLog log (fromLoggable ERROR <> T.pack " " <> fromLoggable error)
+        stopLog log
         exitSuccess
        Right _ -> msgLog log (fromLoggable INFO <> T.pack " " <> fromLoggable " OK ")
 
--- src/Telegram/Api.hs
+++ src/Telegram/Api.hs
@@ -1,23 +1,24 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase     #-}
 module Telegram.Api where
 
-import Control.Monad.Reader (ReaderT)
+import           Control.Monad.Reader     (ReaderT)
 
-import Error (BotError)
+import           Error                    (BotError)
 
-import Control.Error (ExceptT)
-import  qualified Data.ByteString.Char8 as BS8
-import Logger.Class (Logger, logI)
-import Telegram.Types (TelegramOpts(..), Updates(..),Update'(..), GetUpdates(..))
-import Control.Monad.State.Lazy (StateT)
-import qualified Data.Map as Map
-import Control.Monad.RWS.Class (asks, get, modify)
-import Data.Has (getter)
-import Telegram.Class (toAPI)
-import Control.Monad.Cont (unless)
-import Telegram.Types --(Url(..), TelegramOpts(..), GetUpdates(..), Updates(..), SendMessage(..), Update(..),Message(..), From(..),)
-import App (prepareAnswer)
+import           App                      (prepareAnswer)
+import           Control.Error            (ExceptT)
+import           Control.Monad.Cont       (unless)
+import           Control.Monad.RWS.Class  (asks, get, modify)
+import           Control.Monad.State.Lazy (StateT)
+import qualified Data.ByteString.Char8    as BS8
+import           Data.Has                 (getter)
+import qualified Data.Map                 as Map
+import           Logger.Class             (Logger, logI)
+import           Telegram.Class           (toAPI)
+import Telegram.Types
+
+
 telegram :: ExceptT BotError (ReaderT (Logger IO, TelegramOpts) (StateT (Map.Map Integer Integer) IO)) ()
 telegram  = do
   logI "bot launched"
@@ -26,8 +27,9 @@ telegram  = do
 getUpdates :: Maybe Integer -> Maybe Integer -> Maybe Integer -> Maybe [String] -> ExceptT BotError (ReaderT (Logger IO, TelegramOpts) (StateT (Map.Map Integer Integer) IO)) ()
 getUpdates  o l t a_u  = do
       TelegramOpts _ _ btn  <- asks getter
-      logI "awaiting message"
+      
       (Updates update) <- toAPI $ GetUpdates o l t a_u
+      logI "awaiting message"
       unless (null update) do
         user <- get
         mapM_
@@ -42,11 +44,11 @@ getUpdates  o l t a_u  = do
                -> do logI "callback "
                      modify (Map.insert (fromId(cbFrom callback)) (read(cbData callback)))
                      logI  "send message"
-                     toAPI $ SendMessage (fromId(cbFrom callback)) " good " (BS8.pack "") 
+                     toAPI $ SendMessage (fromId(cbFrom callback)) " good " (BS8.pack "")
           )
           update
-        let last_id = update_id (last update) 
+        let last_id = update_id (last update)
         getUpdates (Just (last_id + 1 :: Integer )) Nothing (Just 20) Nothing
-      getUpdates (Just 20) Nothing (Just 20) Nothing  
+      getUpdates (Just 20) Nothing (Just 20) Nothing
 
 
